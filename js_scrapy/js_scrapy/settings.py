9c703fe70f79b0a716b8a215e93fe35607e4c519
hendra <hendradev8@gmail.com>
2021-03-04 16:18:48 +0700

    remove selenium config

--- js_scrapy/js_scrapy/settings.py
+++ js_scrapy/js_scrapy/settings.py
@@ -53,11 +53,6 @@ SPIDER_MIDDLEWARES = {
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 
-SELENIUM_DRIVER_NAME = 'chrome'
-SELENIUM_DRIVER_EXECUTABLE_PATH = '../chromedriver'
-#SELENIUM_DRIVER_ARGUMENTS=['--headless', '--no-sandbox', '--disable-gpu']
-SELENIUM_DRIVER_ARGUMENTS=['--headless']
-
 SPLASH_URL = 'http://localhost:8050'
 DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
 
@@ -66,7 +61,6 @@ DOWNLOADER_MIDDLEWARES = {
     'scrapy_splash.SplashCookiesMiddleware': 723,
     'scrapy_splash.SplashMiddleware': 725,
     'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
-    'scrapy_selenium.SeleniumMiddleware': 800,
 }
 
 # Enable or disable extensions
